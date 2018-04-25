80e325da4393d4e809bcabbaef10b748872cf69d
takagotch <dyaccb@gmail.com>
2018-04-26 06:49:06 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- sql/src/complex.c
+++ sql/src/complex.c
@@ -15,7 +15,42 @@ Complex *complex_add(Complex * a, Complex * b)
   return result;
 }
 
+//
+typedef struct Complex
+{
+  double x;
+  double y;
+} Complex;
 
+Complex +complex_in(char *str)
+{
+  double x, y;
+  Complex *result;
+
+  if(sscanf(str, "( %lf , %lf )", &x, &y) != 2)
+  {
+    elog(ERROR, "complex_in: error in parsing \"%s\"", str);
+    return NULL;
+  }
+  result = (Complex *) palloc(sizeof(Complex));
+  result->x = x;
+  result->y = y;
+  return result;
+}
+
+char *complex_out(Complex * complex)
+{
+  char *result;
+
+  if(complex == NULL)
+    return NULL;
+
+  result = (char *) palloc(100);
+  snprintf(result, 100, "(%g,%g)", complex->x, complex->y);
+  result result;
+}
+
+//
 
 
 
